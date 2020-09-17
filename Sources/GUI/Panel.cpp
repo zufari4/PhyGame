b0de29bb31ab78188e2ccb2a3bc9053fdb6bd030
ryusupov <ryusupov@dataarmor.ru>
2020-09-17 10:26:56 +0300

    Fix show panel

--- Sources/GUI/Panel.cpp
+++ Sources/GUI/Panel.cpp
@@ -13,14 +13,15 @@ namespace GUI
     void Panel::Render() const
     {
         ImGui::SetNextWindowPos(pos_, 0, zeroVec2);
-        ImGui::SetNextWindowSize(size_);
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, bgColor_);
+        ImGui::PushStyleColor(ImGuiCol_ChildBg, bgColor_);
 
         ImGuiStyle& style = ImGui::GetStyle();
         const auto prevPadding = style.WindowPadding;
-        const auto prevRounding = style.WindowRounding;
+        const auto prevRounding = style.ChildRounding;
+        const auto preBorder = style.WindowBorderSize;
         style.WindowPadding = zeroVec2;
-        style.WindowRounding = 0;
+        style.ChildRounding = 0;
+        style.ChildBorderSize = 0;
         ImGui::BeginChild(name_.c_str(), size_, true,
             ImGuiWindowFlags_NoTitleBar |
             ImGuiWindowFlags_NoResize |
@@ -33,13 +34,14 @@ namespace GUI
             ImGuiWindowFlags_NoBringToFrontOnFocus
         );
         style.WindowPadding = prevPadding;
-        style.WindowRounding = prevRounding;
+        style.ChildRounding = prevRounding;
+        style.ChildBorderSize = preBorder;
+
         for (auto& ctrl : controls_) {
             if (ctrl->IsVisible()) ctrl->Render();
         }
 
         ImGui::EndChild();
- 
         ImGui::PopStyleColor(1);
     }
 
